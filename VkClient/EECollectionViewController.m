737f26e26619bbfe419a81647038fc34028aad14
dariyaprok <dariya_prok@ukr.net>
2015-08-17 01:41:38 +0300

    implemented swapping photos

--- VkClient/EECollectionViewController.m
+++ VkClient/EECollectionViewController.m
@@ -11,6 +11,7 @@
 @interface EECollectionViewController()
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionViewOfPhotos;
 @property (strong, nonatomic) EEVkClientManager* manager;
+@property (strong,nonatomic) UIImageView* bigImageView;
 @end
 
 @implementation EECollectionViewController
@@ -38,17 +39,61 @@
 
 -(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
     UICollectionViewCell *cell = [self.collectionViewOfPhotos cellForItemAtIndexPath:indexPath];
-    UIImageView* bigImageView = [[UIImageView alloc] initWithFrame:CGRectMake(cell.frame.origin.x, cell.frame.origin.y, cell.frame.size.width, cell.frame.size.height)];
-    [bigImageView hnk_setImageFromURL: [NSURL URLWithString:self.manager.linksForSmallPhotos[indexPath.row]]];
-    bigImageView.contentMode = UIViewContentModeScaleAspectFit;
-    [self.view addSubview:bigImageView];
+    self.bigImageView = [[UIImageView alloc] initWithFrame:CGRectMake(cell.frame.origin.x, cell.frame.origin.y, cell.frame.size.width, cell.frame.size.height)];
+    [self addImageWithNumber:indexPath.row];
+    
+    
+}
+static NSInteger numberOfPicture;
+-(void)addImageWithNumber: (NSInteger) number {
+    numberOfPicture = number;
+    [self.bigImageView hnk_setImageFromURL: [NSURL URLWithString:self.manager.linksForSmallPhotos[number]]];
+    self.bigImageView.contentMode = UIViewContentModeScaleAspectFit;
+    self.bigImageView.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:self.bigImageView];
     [UIView animateWithDuration:2 animations:^{
-        bigImageView.backgroundColor = [UIColor colorWithRed:0.29 green:0.29 blue:0.29 alpha:0.9];
-        bigImageView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
-        [bigImageView hnk_setImageFromURL:[NSURL URLWithString:self.manager.linksForBigPhotos[indexPath.row]]];
+        
+        self.bigImageView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+        
     }];
+    self.bigImageView.backgroundColor = [UIColor colorWithRed:0.29 green:0.29 blue:0.29 alpha:0.9];
+    [self.bigImageView hnk_setImageFromURL:[NSURL URLWithString:self.manager.linksForBigPhotos[number]]];
+    self.bigImageView.userInteractionEnabled = YES;
+    UITapGestureRecognizer* tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapped)];
+    [self.bigImageView addGestureRecognizer:tap];
+    UISwipeGestureRecognizer *swipeLeft = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipe:)];
+    UISwipeGestureRecognizer *swipeRight = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipe:)];
+
+    [swipeLeft setDirection:UISwipeGestureRecognizerDirectionLeft];
+    [swipeRight setDirection:UISwipeGestureRecognizerDirectionRight];
+    [self.bigImageView addGestureRecognizer:swipeLeft];
+    [self.bigImageView addGestureRecognizer:swipeRight];
+}
+
+-(void)tapped {
+    [self.bigImageView removeFromSuperview];
+    numberOfPicture = nil;
+}
+
+- (void)handleSwipe:(UISwipeGestureRecognizer *)swipe {
+    if (swipe.direction == UISwipeGestureRecognizerDirectionRight) {
+        if(numberOfPicture == 0) {
+            numberOfPicture = self.manager.linksForSmallPhotos.count - 1;
+        }
+        else {
+            numberOfPicture--;
+        }
+        [self addImageWithNumber:numberOfPicture];
+    }
     
-    
-    
+    if (swipe.direction == UISwipeGestureRecognizerDirectionLeft) {
+        if(numberOfPicture == self.manager.linksForSmallPhotos.count-1) {
+            numberOfPicture = 0;
+        }
+        else {
+            numberOfPicture++;
+        }
+        [self addImageWithNumber:numberOfPicture];;
+    }
 }
 @end
